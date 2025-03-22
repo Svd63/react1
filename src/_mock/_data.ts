62805dc93df9928d4573279ec1cc09fd5663970f
Svd63 <Svd63@ioiio.cc>
2025-03-22 16:50:20 +0700

    update path

--- src/_mock/_data.ts
+++ src/_mock/_data.ts
@@ -16,7 +16,7 @@ import {
 export const _myAccount = {
   displayName: 'Jaydon Frankie',
   email: 'demo@minimals.cc',
-  photoURL: '/assets/images/avatar/avatar-25.webp',
+  photoURL: 'assets/images/avatar/avatar-25.webp',
 };
 
 // ----------------------------------------------------------------------
@@ -26,7 +26,7 @@ export const _users = [...Array(24)].map((_, index) => ({
   name: _fullName(index),
   company: _company(index),
   isVerified: _boolean(index),
-  avatarUrl: `/assets/images/avatar/avatar-${index + 1}.webp`,
+  avatarUrl: `assets/images/avatar/avatar-${index + 1}.webp`,
   status: index % 4 ? 'active' : 'banned',
   role:
     [
@@ -49,7 +49,7 @@ export const _posts = [...Array(23)].map((_, index) => ({
   id: _id(index),
   title: _postTitles(index),
   description: _description(index),
-  coverUrl: `/assets/images/cover/cover-${index + 1}.webp`,
+  coverUrl: `assets/images/cover/cover-${index + 1}.webp`,
   totalViews: 8829,
   totalComments: 7977,
   totalShares: 8556,
@@ -57,7 +57,7 @@ export const _posts = [...Array(23)].map((_, index) => ({
   postedAt: _times(index),
   author: {
     name: _fullName(index),
-    avatarUrl: `/assets/images/avatar/avatar-${index + 1}.webp`,
+    avatarUrl: `assets/images/avatar/avatar-${index + 1}.webp`,
   },
 }));
 
@@ -82,7 +82,7 @@ export const _products = [...Array(24)].map((_, index) => {
     price: _price(index),
     name: _productNames(index),
     priceSale: setIndex % 3 ? null : _price(index),
-    coverUrl: `/assets/images/product/product-${setIndex}.webp`,
+    coverUrl: `assets/images/product/product-${setIndex}.webp`,
     colors:
       (setIndex === 1 && COLORS.slice(0, 2)) ||
       (setIndex === 2 && COLORS.slice(1, 3)) ||
@@ -102,17 +102,17 @@ export const _langs = [
   {
     value: 'en',
     label: 'English',
-    icon: '/assets/icons/flags/ic-flag-en.svg',
+    icon: 'assets/icons/flags/ic-flag-en.svg',
   },
   {
     value: 'de',
     label: 'German',
-    icon: '/assets/icons/flags/ic-flag-de.svg',
+    icon: 'assets/icons/flags/ic-flag-de.svg',
   },
   {
     value: 'fr',
     label: 'French',
-    icon: '/assets/icons/flags/ic-flag-fr.svg',
+    icon: 'assets/icons/flags/ic-flag-fr.svg',
   },
 ];
 
